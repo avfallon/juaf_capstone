fdd1aa5f1bef8509bb60532f846ef23948335a90
avfallon <31700889+avfallon@users.noreply.github.com>
2021-04-19 13:08:27 -0400

    acountid

--- php/query.php
+++ php/query.php
@@ -4,7 +4,6 @@
  echo $username;
  // credentials to connect to sqli
  $mysqli = new mysqli( Constants::HOST, $username, Constants::PASSWORD, Constants::DATABASE);
- echo "hello";
 
  $result = $mysqli->query("select * from user_info");
 
@@ -14,8 +13,8 @@
  {
   // assign the values into the json object
    array_push($array, [
-     'id' => $row['OwnerIDKey'],
-     'account_name' => $row['id'],
+     'id' => $row['id'],
+     'account_name' => $row['account_name'],
      'account_email' => $row['account_email'],
      'current_funds' => $row['current_funds'],
      'recent_purcahse' => $row['recent_purchase'],
