94dd47547f02e3fec4260d04718db206cd2cf233
avfallon <avfallon@loyola.edu>
2021-04-25 14:30:56 -0400

    storing changes

--- php/addAccount.php
+++ php/addAccount.php
@@ -13,25 +13,32 @@
   // Access values from the returned object
 
 
-  // get each field from the json to create pet
+  // get each field from the json to create account
   $account_name = $obj->account_name;
-  echo $account_name
+  echo $account_name;
   $account_email = $obj->account_email;
-  echo $accoun_email
+  echo $account_email;
   $current_funds = $obj->current_funds;
-  echo $current_funds
+  echo $current_funds;
   $recent_purchase = $obj->recent_purchase;
-  echo $recent_purchase
+  echo $recent_purchase;
+  $username = $obj->username;
+  echo $username;
+  $password = $obj->password;
+  echo $password
 
 
-  // sql query to the database to create the pet
+
+  // sql query to the database to create the account_info row
   $query = "insert into user_info (account_name, account_email, current_funds, recent_purchase) values('$account_name', '$account_email', '$current_funds', '$recent_purchase')";
 
   // run the query
   $result = $mysqli->query($query);
 
   $id = $mysqli->query("select id from user_info where account_email = '$account_email';");
-  echo $id
+  echo $id;
+
+// Use that id to add the username/password to the login_info table
 
   echo "Account Added Sucessfully";
 ?>
--- php/getEmailInfo.php
+++ php/getEmailInfo.php
@@ -1,31 +1,27 @@
 <?php
  include( "constants.php");
  $username = Constants::USERNAME;
- echo $username;
- echo "\n";
+
  // credentials to connect to sqli
  $mysqli = new mysqli( Constants::HOST, $username, Constants::PASSWORD, Constants::DATABASE);
 
- $result = $mysqli->query("select * from user_info");
+ $result = $mysqli->query("select account_email, current_funds from Pets");
 
- // make an array of json account objects
+ // make an array of json email and fund data
  $array = [];
  while($row = $result->fetch_assoc())
  {
   // assign the values into the json object
    array_push($array, [
-     'id' => $row['id'],
-     'account_name' => $row['account_name'],
      'account_email' => $row['account_email'],
      'current_funds' => $row['current_funds'],
-     'recent_purcahse' => $row['recent_purchase'],
    ]);
  }
 
  //encode the array into json
  $result = json_encode($array);
 
- //reurn the array of accounts in the database
+ //reurn the array of emails and fund information in the database
  echo $result;
- echo "\n";
+
  ?>
