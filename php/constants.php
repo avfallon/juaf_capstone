de21847ff425f06824be48297674ae8dd15116c5
avfallon <31700889+avfallon@users.noreply.github.com>
2021-05-03 22:54:48 -0400

    constants

---
+++ php/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+// This file contains constants which will be used as credentials for every query
+class Constants
+{
+        const USERNAME = 
+        const HOST =
+        const DATABASE =
+        const PASSWORD =
+}
+
+?>
